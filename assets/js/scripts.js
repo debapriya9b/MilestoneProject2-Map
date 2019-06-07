15d64769fb83e79387b3b91e846bad4f757e4a11
Debapriya Bhattacharya <debapriya9b@gmail.com>
2019-06-07 23:03:11 +0000

    marker changed to dots

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 //Creating map options
 var mapOptions = {
-    center: [53.4239, -7.9407],
+    center: [40.4168, -3.7038],
     zoom: 2,
     minZoom: 2,
     maxZoom: 18
@@ -19,14 +19,26 @@ map.addLayer(layer);
 
 //Adding geoJson data
 
-
+var geojsonMarkerOptions = {
+    radius: 3,
+    fillColor: "#F08080",
+    color: "#000",
+    weight: 1,
+    opacity: 0.2,
+    fillOpacity: 0.8
+};
 
 function volcanoSearch(feature, layer) {
     layer.bindPopup("<h6>Name:" + feature.properties.NAME_ + "</h6><p>Type:" + feature.properties.TYPE_ + "</p>");
+    
 
 };
 
 L.geoJson(volcano, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+            
+        },
     onEachFeature: volcanoSearch
 }).addTo(map);
 
