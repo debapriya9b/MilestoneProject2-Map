c77801e882b3ea20c46bf17c10d9658416054b33
Debapriya Bhattacharya <debapriya9b@gmail.com>
2019-06-19 20:39:08 +0000

    leaflet features added for map

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,7 +19,12 @@ var mapOptions = {
     center: [40.4168, -3.7038],
     zoom: 2,
     minZoom: 2,
-    maxZoom: 18
+    maxZoom: 18,
+    maxBounds: [
+                [-75, -190],
+                [90, 190]
+            ],
+            maxBoundsViscosity: 0.5,
 }
 
 // Creating a map object
